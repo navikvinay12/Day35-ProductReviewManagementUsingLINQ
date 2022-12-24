73c5d71b4518347486b597a900e117ce9f8089a1
Vinay <navik.vinay12@gmail.com>
2022-12-24 21:53:13 +0530

    method to fetch top 3 record based on ratings

---
+++ ProductReviewManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo222Batch
+{
+    public class ProductReviewManagement
+    {
+        public static void RetrieveTop3BasedOnRating(List<ProductReview> list)  //UC2
+        {
+            Console.WriteLine("\n Retrieve Top3 Records Based on Rating");
+            var top3Records = list.OrderByDescending(p => p.Rating).Take(3).ToList();
+            Program.DisplayProducts(top3Records);
+
+            //Console.WriteLine("\n Using query syntax");
+            //var res = (from product in list orderby product.Rating descending select product).Take(3).ToList();
+            //Program.DisplayProducts(res);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,23 +16,24 @@
                 new ProductReview() { ProductId = 3, UserId = 8, IsLike = true, Review = "good", Rating = 81 },
                 new ProductReview() { ProductId = 1, UserId = 9, IsLike = false, Review = "bad", Rating = 25 },
                 new ProductReview() { ProductId = 13, UserId = 10, IsLike = true, Review = "average", Rating = 53 },
-                new ProductReview() { ProductId = 20, UserId = 11, IsLike = true, Review = "nice", Rating = 90 },
+                new ProductReview() { ProductId = 20, UserId = 11, IsLike = true, Review = "nice", Rating = 91 },
                 new ProductReview() { ProductId = 18, UserId = 12, IsLike = false, Review = "bad", Rating = 33 },
                 new ProductReview() { ProductId = 16, UserId = 13, IsLike = false, Review = "bad", Rating = 34 },
                 new ProductReview() { ProductId = 19, UserId = 14, IsLike = false, Review = "bad", Rating = 36 },
                 new ProductReview() { ProductId = 20, UserId = 15, IsLike = true, Review = "good", Rating = 82 },
                 new ProductReview() { ProductId = 11, UserId = 16, IsLike = false, Review = "bad", Rating = 37 },
                 new ProductReview() { ProductId = 17, UserId = 17, IsLike = true, Review = "average", Rating = 52 },
-                new ProductReview() { ProductId = 23, UserId = 18, IsLike = true, Review = "nice", Rating = 90 },
+                new ProductReview() { ProductId = 23, UserId = 18, IsLike = true, Review = "nice", Rating = 92 },
                 new ProductReview() { ProductId = 11, UserId = 19, IsLike = false, Review = "bad", Rating = 38 },
                 new ProductReview() { ProductId = 15, UserId = 20, IsLike = false, Review = "bad", Rating = 39 },
                 new ProductReview() { ProductId = 19, UserId = 21, IsLike = false, Review = "bad", Rating = 30 },
                 new ProductReview() { ProductId = 21, UserId = 22, IsLike = true, Review = "good", Rating = 83 },
                 new ProductReview() { ProductId = 15, UserId = 23, IsLike = false, Review = "bad", Rating = 35 },
                 new ProductReview() { ProductId = 13, UserId = 24, IsLike = true, Review = "average", Rating = 51 },
-                new ProductReview() { ProductId = 22, UserId = 25, IsLike = true, Review = "nice", Rating = 90 }
+                new ProductReview() { ProductId = 22, UserId = 25, IsLike = true, Review = "nice", Rating = 93 }
             };
             DisplayProducts(list);
+            ProductReviewManagement.RetrieveTop3BasedOnRating(list);        //UC2
         }
         public static void DisplayProducts(List<ProductReview> list)
         {
